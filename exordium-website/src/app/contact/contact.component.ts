785097730814772c9a86b18bedf6169f800f17bf
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-03-18 18:42:25 -0400

    Modifications to the contact page (adding slight form functionality)

--- exordium-website/src/app/contact/contact.component.ts
+++ exordium-website/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  contactForm: FormGroup;
+  loading = false;
+  success = '';
+  error = '';
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+      name: [null, Validators.required],
+      email: [null, Validators.required, Validators.email],
+      companyName: [null],
+      phone: [null],
+      message: [null, Validators.required]
+    });
+  }
+
+  get f() {
+    return this.contactForm.controls;
+  }
+
+  onSubmit() {
+    this.loading = true;
+
+
+    // send to api
+
+
+    // Thanks, a member of our team will be in touch shortly.
+    // Please fill all fields correctly.
+    this.error = "Sending emails is currently disabled as this feature hasn't been completed yet.";
   }
 
 }
