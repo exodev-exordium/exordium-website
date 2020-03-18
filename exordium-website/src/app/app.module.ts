7328ee02192a05e01652f2cba0de9dda985ce806
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-03-18 09:58:53 -0400

    Move `/` redirect to be the first route, and add contact and the members pages to routing.

--- exordium-website/src/app/app.module.ts
+++ exordium-website/src/app/app.module.ts
@@ -5,23 +5,41 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ContactComponent } from './contact/contact.component';
+import { SigninComponent } from './members/signin/signin.component';
+import { RegisterComponent } from './members/register/register.component';
 
 const appRoutes : Routes = [
+  {
+    path: '',
+    redirectTo: '/welcome',
+    pathMatch: 'full'
+  },
   {
     path: 'welcome',
     component: WelcomeComponent
   },
   {
-    path: '',
-    redirectTo: '/welcome',
-    pathMatch: 'full'
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'members/signin',
+    component: SigninComponent
+  },
+  {
+    path: 'members/register',
+    component: RegisterComponent
   }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    ContactComponent,
+    SigninComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
