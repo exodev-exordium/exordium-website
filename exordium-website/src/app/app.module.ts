5eba91733d30647476c47b5aeb55e4cf6a81d372
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-03-20 04:28:41 -0400

    Add SVG Injector file dividers and other images, and fix the directories for the moved pages.

--- exordium-website/src/app/app.module.ts
+++ exordium-website/src/app/app.module.ts
@@ -3,23 +3,28 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+// SVG Inline Injector
+import { HttpClientModule } from '@angular/common/http';
+import { InlineSVGModule } from 'ng-inline-svg';
+
+// Main App Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // Error Reporting
-import { NotFoundComponent } from './error/not-found/not-found.component';
-import { ForbiddenComponent } from './error/forbidden/forbidden.component';
+import { NotFoundComponent } from './pages/error/not-found/not-found.component';
+import { ForbiddenComponent } from './pages/error/forbidden/forbidden.component';
 
 // Welcome Page
-import { WelcomeComponent } from './welcome/welcome.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 // Company Pages
-import { AboutUsComponent } from './company/about-us/about-us.component';
-import { ContactComponent } from './company/contact/contact.component';
+import { AboutUsComponent } from './pages/company/about-us/about-us.component';
+import { ContactComponent } from './pages/company/contact/contact.component';
 
 // Members Pages
-import { SigninComponent } from './members/signin/signin.component';
-import { RegisterComponent } from './members/register/register.component';
+import { SigninComponent } from './pages/members/signin/signin.component';
+import { RegisterComponent } from './pages/members/register/register.component';
 
 const appRoutes : Routes = [
   {
@@ -100,9 +105,13 @@ const appRoutes : Routes = [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule, 
+    InlineSVGModule.forRoot(),
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: true }
+      { 
+        enableTracing: true 
+      }
     )
   ],
   providers: [],
