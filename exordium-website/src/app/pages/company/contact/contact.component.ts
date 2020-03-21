422794434330454f2e7988b2ab8db5d7ec2a1265
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-03-20 20:34:48 -0400

    Redesigned contact page, and changed from default angular code to jquery and bootstrap-notify.

--- exordium-website/src/app/pages/company/contact/contact.component.ts
+++ exordium-website/src/app/pages/company/contact/contact.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import * as jQuery from 'jquery';
+import 'bootstrap-notify';
+
+let $ = jQuery;
 
 @Component({
   selector: 'app-contact',
@@ -8,11 +13,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ContactComponent implements OnInit {
   contactForm: FormGroup;
-  loading = false;
+
   success = '';
   error = '';
 
   constructor(
+    private renderer: Renderer2,
     private formBuilder: FormBuilder
   ) { }
 
@@ -31,20 +37,57 @@ export class ContactComponent implements OnInit {
   }
 
   onSubmit() {
-    this.loading = true;
+    var sourceButton = $(".submitContact");
 
+    sourceButton.attr("disabled", true);
+    sourceButton.addClass("m-progress");
+
+    var notify = $.notify({
+      icon: 'fa fa-fw fa-info-circle',
+      message: '<strong>Sending your contact request!</strong> This could take a moment...'
+    },{
+      type: 'primary',
+      placement: {
+        from: "bottom",
+        align: "right"
+      },
+      showProgressbar: true,
+      delay: 10000,
+      animate: {
+        enter: 'animated fadeInDown',
+        exit: 'animated fadeOutUp'
+      },
+      template: '<div data-notify="container" class="notify-width alert alert-{0}" role="alert">' +
+        '<span data-notify="icon"></span> ' +
+        '<span data-notify="title">{1}</span> ' +
+        '<span data-notify="message">{2}</span>' +
+        '<a href="{3}" target="{4}" data-notify="url"></a>' +
+        '<div class="progress notifyAlert" data-notify="progressbar">' +
+          '<div class="progress-bar bg-{0}" role="progressbar" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="0"></div>' +
+        '</div>' +
+      '</div>'
+    });
+
+    // test the data
     if (this.contactForm.invalid) {
-      this.loading = false;
-      return;
+      notify.update({
+        'type': 'danger',
+        'icon': 'fa fa-fw fa-times',
+        'message': `<strong>Error!</strong> Please make sure you enter all the fields properly.`
+      });
+    } else {
+
+      notify.update({
+        'type': 'danger',
+        'icon': 'fa fa-fw fa-times',
+        'message': `<strong>Error!</strong> Sending emails is currently disabled as this feature hasn't been completed yet.`
+      });
     }
 
-
-    // send to api
-
-
-    // Thanks, a member of our team will be in touch shortly.
-    // Please fill all fields correctly.
-    this.error = "Sending emails is currently disabled as this feature hasn't been completed yet.";
+    setTimeout(function () {
+      sourceButton.removeAttr("disabled");
+      sourceButton.removeClass("m-progress");
+    }, 2000);
 
   }
 
