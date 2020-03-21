3dcb4f2ebd0988101c484d52141bfc92be65b908
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-03-21 04:02:18 -0400

    Initial Header Primary Component

---
+++ exordium-website/src/app/components/header/primary/primary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-primary',
+  templateUrl: './primary.component.html',
+  styleUrls: ['./primary.component.scss']
+})
+export class PrimaryHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
