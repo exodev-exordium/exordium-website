7e4db12a8e388bc13f9b74ea07b66b6e724f5ea6
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-03-31 05:40:10 -0400

    Initial Main Navbar Component commit

---
+++ exordium-website/src/app/components/navbar/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
