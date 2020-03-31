d9af6038a9f81ac21d25a24eed0695d54ef8c086
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-03-31 18:18:29 -0400

    Proper header functionality

--- exordium-website/src/app/components/header/overlay/overlay.component.ts
+++ exordium-website/src/app/components/header/overlay/overlay.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+import * as jQuery from 'jquery';
+let $ = jQuery;
 
 @Component({
   selector: 'app-header-overlay',
@@ -6,15 +10,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overlay.component.scss']
 })
 export class OverlayHeaderComponent implements OnInit {
+  windowScrolled: boolean;
 
-  constructor() { }
+  constructor(
+    @Inject (DOCUMENT) private document: Document
+  ) { }
 
   ngOnInit(): void {
+    this.invertMargin();
+    this.stickyTop();
+
+    $('.navbar-toggler').click(function() {
+      $('nav.navbar').toggleClass("navbar-open");
+    });
   }
 
-  // should stick the the top of the window
-  // should have the first section underneath it (margin-bottom: -#px )
-  
+  @HostListener("window:resize", [])
+  onResize() {
+    this.invertMargin();
+    this.stickyTop();
+  }
+
+  invertMargin() {
+    // Invert Margin
+    let navHeight = $('nav.navbar').outerHeight();
+
+    $('nav.navbar').parent().css({
+      "min-height": `${navHeight}px`,
+      "margin-bottom": `-${navHeight}px`
+    });
+
+    // Spacing for AboutNav
+    $('.aboutNav').css("top", `${navHeight}px`);
+  }
+
+  stickyTop() {
+    // Stick to the top
+    let windowWidth = $(window).width();
+
+    $('nav').css({
+      "top": 0,
+      "max-width": `${windowWidth}px`
+    });
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    if (window.pageXOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+      this.windowScrolled = true;
+
+      if (!$('nav.navbar').hasClass("scrolled")) {
+        $('nav.navbar').addClass("position-fixed scrolled");
+      }
+
+    } else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+      this.windowScrolled = true;
+
+      if ($('nav.navbar').hasClass("scrolled")) {
+        $('nav.navbar').removeClass("position-fixed scrolled");
+      }
+    }
+  }
+
+  // should stick the the top of the window  
   // as the window scrolls down it should have bg-primary-3 as the background
 
 }
--- exordium-website/src/app/components/header/primary/primary.component.ts
+++ exordium-website/src/app/components/header/primary/primary.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+import * as jQuery from 'jquery';
+let $ = jQuery;
 
 @Component({
   selector: 'app-header-primary',
@@ -6,12 +10,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./primary.component.scss']
 })
 export class PrimaryHeaderComponent implements OnInit {
+  windowScrolled: boolean;
 
-  constructor() { }
+  constructor(
+    @Inject (DOCUMENT) private document: Document
+  ) { }
 
   ngOnInit(): void {
+    this.addMargin();
+    this.stickyTop();
   }
 
-  // should have the first section underneath it (margin-bottom: -#px )
+  @HostListener("window:resize", [])
+  onResize() {
+    this.addMargin();
+    this.stickyTop();
+  }
+
+  addMargin() {
+        // Invert Margin
+        let navHeight = $('nav.navbar').outerHeight();
+
+        $('nav.navbar').parent().css({
+          "min-height": `${navHeight}px`
+        });
+    
+        // Spacing for AboutNav
+        $('.aboutNav').css("top", `${navHeight}px`);
+  }
+
+  stickyTop() {
+    // Stick to the top
+    let windowWidth = $(window).width();
+
+    $('nav').css({
+      "top": 0,
+      "max-width": `${windowWidth}px`
+    });
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    if (window.pageXOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+      this.windowScrolled = true;
+
+      if (!$('nav.navbar').hasClass("scrolled")) {
+        $('nav.navbar').addClass("position-fixed scrolled");
+      }
+
+    } else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+      this.windowScrolled = true;
+
+      if ($('nav.navbar').hasClass("scrolled")) {
+        $('nav.navbar').removeClass("position-fixed scrolled");
+      }
+    }
+  }
 
 }
