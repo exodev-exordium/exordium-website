e011b4d937d1033df74a610144970a7695602e5e
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-04-02 02:57:35 -0400

    Dashboard module and routing

---
+++ exordium-website/src/app/pages/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Auth Guard
+import { AuthGuard } from 'src/app/service/auth.guard';
+
+// Components
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingsComponent } from './settings/settings.component';
+
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    data: {
+      title: 'Dashboard'
+    },
+    //canActivate: [AuthGuard]
+  },
+  {
+    path: 'dashboard/profile',
+    component: ProfileComponent,
+    data: {
+      title: 'Profile'
+    },
+    //canActivate: [AuthGuard]
+  },
+  {
+    path: 'dashboard/settings',
+    component: SettingsComponent,
+    data: {
+      title: 'Settings'
+    },
+    //canActivate: [AuthGuard]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DashboardRoutingModule { }
