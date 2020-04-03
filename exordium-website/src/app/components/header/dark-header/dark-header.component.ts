6cbcb4a18f63656ebfb0da3dac1e53506194ee63
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-04-02 23:41:10 -0400

    Change primary to dark header

---
+++ exordium-website/src/app/components/header/dark-header/dark-header.component.ts
@@ -0,0 +1,111 @@
+import { Component, OnInit, Inject, HostListener, Host } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AuthService } from 'src/app/service/auth.service';
+
+import * as jQuery from 'jquery';
+let $ = jQuery;
+
+@Component({
+  selector: 'app-dark-header',
+  templateUrl: './dark-header.component.html',
+  styleUrls: ['./dark-header.component.scss']
+})
+export class DarkHeaderComponent implements OnInit {
+  // User Signed in?
+  signedIn: boolean;
+
+  // Window being scrolled?
+  windowScrolled: boolean;
+
+  constructor(
+    @Inject (DOCUMENT) private document: Document,
+    private authService: AuthService
+  ) { }
+
+  ngOnInit(): void {
+    this.checkSignedIn();
+
+    // Header Options
+    this.addFraming();
+    this.stickyTop();
+  }
+
+  checkSignedIn () {
+    if (this.authService.isSignedIn === true) {
+      this.signedIn = true;
+    } else {
+      this.signedIn = false;
+    }
+  }
+
+  addFraming () {
+    // Main Navbar Height
+    let mainNavHeight = $('nav.navbar').outerHeight();
+
+    // Dashboard Navbar Height
+    let dashboardNavHeight = $('.nav-dashboard-wrapper').outerHeight();
+
+    // Main Nav Framing
+    $('nav.navbar').parent().css({
+      "min-height": `${mainNavHeight}px`
+    });
+
+    // About Nav Spacing
+    $('.aboutNav').css({
+      "top": `${mainNavHeight}px`
+    });
+
+    // Dashboard Menu Framing
+    $('.nav-dashboard-wrapper').parent().css({
+      "min-height": `${dashboardNavHeight}px`
+    });
+  }
+
+  stickyTop () {
+    let windowWidth = $(window).width();
+
+    // Main Nav
+    $('nav.navbar').css({
+      "top": 0,
+      "max-width": `${windowWidth}px`
+    });
+    
+    // Dashboard Nav
+    $('.nav-dashboard-wrapper').css({
+      "max-width": `${windowWidth}px`
+    });
+  }
+
+  @HostListener("window:resize", [])
+  onResize () {
+    this.addFraming();
+    this.stickyTop();
+  }
+
+  @HostListener("window:scroll", [])
+  onScroll () {
+    if (window.pageXOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+      this.windowScrolled = true;
+
+      if (!$('nav.navbar').hasClass("scrolled")) {
+        $('nav.navbar').addClass("position-fixed scrolled");
+      }
+
+      if (!$('.nav-dashboard-wrapper').hasClass("scrolled")) {
+        $('.nav-dashboard-wrapper').addClass("position-fixed scrolled");
+      }
+
+    } else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+      this.windowScrolled = true;
+
+      if ($('nav.navbar').hasClass("scrolled")) {
+        $('nav.navbar').removeClass("position-fixed scrolled");
+      }
+
+      if ($('.nav-dashboard-wrapper').hasClass("scrolled")) {
+        $('.nav-dashboard-wrapper').removeClass("position-fixed scrolled");
+      }
+    }
+  }
+
+}
