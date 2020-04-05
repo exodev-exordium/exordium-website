c548adeb4f44590f74f3cbdf808d3f1e5b23b891
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-04-05 01:48:41 -0400

    Update the endpoint url, will update again to the live database

--- exordium-website/src/app/service/shared/api.ts
+++ exordium-website/src/app/service/shared/api.ts
@@ -1,6 +1,6 @@
 import { HttpHeaders } from '@angular/common/http';
 
 export class API {
-    public endpoint: string = 'http://localhost:3000/api/v1';
+    public endpoint: string = 'http://localhost:3000';
     public headers = new HttpHeaders().set('Content-Type', 'application/json');
 }
