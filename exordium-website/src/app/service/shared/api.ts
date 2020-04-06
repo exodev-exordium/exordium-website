11bdf64e7eaeb051ccc52f993494eb6b9942ef4e
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-04-06 05:14:53 -0400

    Change API to the domain

--- exordium-website/src/app/service/shared/api.ts
+++ exordium-website/src/app/service/shared/api.ts
@@ -1,6 +1,6 @@
 import { HttpHeaders } from '@angular/common/http';
 
 export class API {
-    public endpoint: string = 'http://localhost:3000';
+    public endpoint: string = 'https://api.exordium.dev'; // http://localhost:3000
     public headers = new HttpHeaders().set('Content-Type', 'application/json');
 }
