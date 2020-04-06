432290e790987fa3ea342c98d05d2304dc26165d
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-04-06 04:57:08 -0400

    Collect user data and provide for component - added cover, live connections

--- exordium-website/src/app/pages/dashboard/dashboard/dashboard.component.ts
+++ exordium-website/src/app/pages/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/service/auth.service';
+import { User } from 'src/app/service/shared/user';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  currentUser = new User();
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { 
+    this.authService.currentUser().subscribe(res => {
+      this.currentUser = res.response;
+    })
+  }
 
   ngOnInit(): void {
   }
