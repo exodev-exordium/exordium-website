66d6407ea70a6adc053215ab00e06b752a8551e8
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-04-06 04:54:23 -0400

    If user data token is invalid, redirect to members/signin

--- exordium-website/src/app/service/authconfig.interceptor.ts
+++ exordium-website/src/app/service/authconfig.interceptor.ts
@@ -1,19 +1,40 @@
 import { Injectable } from "@angular/core";
-import { HttpInterceptor, HttpRequest, HttpHandler } from "@angular/common/http";
+import { Router } from '@angular/router';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse, HttpErrorResponse } from "@angular/common/http";
+import { tap } from "rxjs/operators";
 import { AuthService } from "./auth.service";
 
 @Injectable()
 
 export class AuthInterceptor implements HttpInterceptor {
-    constructor(private authService: AuthService) { }
+    constructor(
+        private router: Router,
+        private authService: AuthService
+        ) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         const authToken = this.authService.getToken();
+
         req = req.clone({
             setHeaders: {
                 Authorization: "Bearer " + authToken
             }
         });
-        return next.handle(req);
+
+        return next.handle(req).pipe(tap(
+            (event: HttpEvent<any>) => {
+                if (event instanceof HttpResponse) {
+                    // token still valid
+                }
+            },
+            (err: any) => {
+                if (err instanceof HttpErrorResponse) {
+                    // token no longer valid
+                    if (err.status === 401) {
+                        this.authService.signout();
+                    }
+                }
+            }
+        ));
     }
 }
