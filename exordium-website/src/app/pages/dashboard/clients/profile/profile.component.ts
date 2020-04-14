6c28ad91a696e756ebb80989b51797fc5d7febeb
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-04-14 14:18:58 -0400

    Profile badge display roles

--- exordium-website/src/app/pages/dashboard/clients/profile/profile.component.ts
+++ exordium-website/src/app/pages/dashboard/clients/profile/profile.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { jarallax } from 'jarallax';
 import { AuthService } from 'src/app/service/auth.service';
 import { Account } from 'src/app/service/shared/account';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -23,6 +24,24 @@ export class ProfileComponent implements OnInit {
     this.authService.getUserData().subscribe(res => {
       this.currentUser = res.response;
     })
+  }
+
+  displayRole(websiteRole) {
+    const role = [{ role: websiteRole }];
+    const apiRoles = this.currentUser.access.roles;
+
+    var result = role.some(function (obj1) {
+      return apiRoles.some(function (obj2) {
+        return obj1.role == obj2.role;
+      });
+    });
+
+    console.log(apiRoles)
+    return result;
+  }
+
+  displayConnected (service) {
+    // function for displaying whether github and discord have been connected
   }
 
 }
