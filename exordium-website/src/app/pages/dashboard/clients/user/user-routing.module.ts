167cb29a7656736f71423dc614803c9935ae6438
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-04-15 07:03:50 -0400

    Dashboard user add the rest of the pages

--- exordium-website/src/app/pages/dashboard/clients/user/user-routing.module.ts
+++ exordium-website/src/app/pages/dashboard/clients/user/user-routing.module.ts
@@ -6,7 +6,10 @@ import { AuthGuard } from 'src/app/service/auth.guard';
 
 // Pages
 import { UserOverviewComponent } from './user-overview/user-overview.component';
-
+import { UserSessionsComponent } from './user-sessions/user-sessions.component';
+import { UserLogsComponent } from './user-logs/user-logs.component';
+import { UserConnectionsComponent } from './user-connections/user-connections.component';
+import { UserSettingsComponent } from './user-settings/user-settings.component';
 
 const routes: Routes = [
   {
@@ -21,6 +24,34 @@ const routes: Routes = [
         component: UserOverviewComponent,
         data: {
           title: 'Overview'
+        }
+      },
+      {
+        path: 'sessions',
+        component: UserSessionsComponent,
+        data: {
+          title: 'Sessions'
+        }
+      },
+      {
+        path: 'logs',
+        component: UserLogsComponent,
+        data: {
+          title: 'Logs'
+        }
+      },
+      {
+        path: 'connections',
+        component: UserConnectionsComponent,
+        data: {
+          title: 'Connections'
+        }
+      },
+      {
+        path: 'settings',
+        component: UserSettingsComponent,
+        data: {
+          title: 'Settings'
         }
       }
     ]
