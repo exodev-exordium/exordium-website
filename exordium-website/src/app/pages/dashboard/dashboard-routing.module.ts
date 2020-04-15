e7908f9545cfc759e44fc44c4f79fe37b591507d
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-04-15 01:08:50 -0400

    Remove old dashboard pages

--- exordium-website/src/app/pages/dashboard/dashboard-routing.module.ts
+++ exordium-website/src/app/pages/dashboard/dashboard-routing.module.ts
@@ -4,14 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 // Auth Guard
 import { AuthGuard } from 'src/app/service/auth.guard';
 
-// Dashboard
+// Pages
 import { DashboardComponent } from './clients/dashboard/dashboard.component';
-import { ProfileComponent } from './clients/profile/profile.component';
-import { SettingsComponent } from './clients/settings/settings.component';
-
-// Moderation
-import { ModContactComponent } from './moderation/mod-contact/mod-contact.component';
-import { ModUsersComponent } from './moderation/mod-users/mod-users.component';
 
 const routes: Routes = [
   {
@@ -19,38 +13,6 @@ const routes: Routes = [
     component: DashboardComponent,
     data: {
       title: 'Dashboard'
-    },
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'dashboard/profile',
-    component: ProfileComponent,
-    data: {
-      title: 'Profile'
-    },
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'dashboard/settings',
-    component: SettingsComponent,
-    data: {
-      title: 'Settings'
-    },
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'moderation/contact',
-    component: ModContactComponent,
-    data: {
-      title: 'Moderation Contact'
-    },
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'moderation/users',
-    component: ModUsersComponent,
-    data: {
-      title: 'Moderation Users'
     },
     canActivate: [AuthGuard]
   }
