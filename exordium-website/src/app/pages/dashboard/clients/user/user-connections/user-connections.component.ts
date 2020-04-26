b6bc34ebd771f16baa2961229c342e56eb5ecf77
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-04-26 03:33:18 -0400

    Detect when there are params set in the url e.g. ?github= or ?discord=

--- exordium-website/src/app/pages/dashboard/clients/user/user-connections/user-connections.component.ts
+++ exordium-website/src/app/pages/dashboard/clients/user/user-connections/user-connections.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from 'src/app/service/user.service';
 
 @Component({
@@ -10,12 +11,35 @@ export class UserConnectionsComponent implements OnInit {
   public currentUser;
 
   constructor(
-    public userService: UserService
-  ) { }
+    public userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { 
+    console.log(location);
+  }
 
   ngOnInit() {
     this.userService.getUserDataBasic().subscribe(res => {
       this.currentUser = res.response;
     });
+
+    // Get Params
+    this.route.queryParams.subscribe(params => {
+      if (params) {
+        if (params.discord) {
+          this.userService.connectDiscord().subscribe(res => {
+            console.log(res);
+            //this.currentUser = res.response;
+          });
+        }
+        if (params.github) {
+          this.userService.connectGithub().subscribe(res => {
+            console.log(res);
+            //this.currentUser = res.response;
+          });
+        }
+      }
+    }); 
   }
+
 }
