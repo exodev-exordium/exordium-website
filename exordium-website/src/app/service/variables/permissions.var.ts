92a6df2c57e353ec0a22af104794a1048621f9ef
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-05-01 06:08:24 -0400

    Permissions Variabls

---
+++ exordium-website/src/app/service/variables/permissions.var.ts
@@ -0,0 +1,44 @@
+export class Permissions {
+
+    public roles = [
+        {
+            role: "user",
+            icon: "fa-user",
+            disabled: true // cant remove user role as its the default
+        },
+        {
+            role: "paid",
+            icon: "fa-money-bill-wave"
+        },
+        {
+            role: "beta",
+            icon: "fa-tools"
+        },
+        {
+            role: "supporter",
+            icon: "fa-users"
+        },
+        {
+            role: "staff",
+            icon: "fa-certificate"
+        },
+        {
+            role: "moderator",
+            icon: "fa-star"
+        },
+        {
+            role: "administrator",
+            icon: "fa-star-shooting"
+        },
+        {
+            role: "developer",
+            icon: "fa-code"
+        }
+    ]
+
+}
+
+export interface Role {
+    role: string;
+    icon: string;
+}
